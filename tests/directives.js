3fb2aba2779344290b322a2e922bed76d8248cda
Nathan Dabu <nathan.dabu@ucf.edu>
2017-11-16 16:27:47 -0500

    added directives tests, blur is only part not covered

--- tests/directives.js
+++ tests/directives.js
@@ -3,45 +3,72 @@ var $scope = {};
 var element = null;
 
 describe('focusMe Directive', function(){
-    var $timeout = null;
-    beforeEach(module('crosswordCreator'));
+	var $timeout = null;
+	beforeEach(module('crosswordCreator'));
 
-    beforeEach(inject(function(_$compile_, $rootScope, _$timeout_){
-        $timeout = _$timeout_;
-        $compile = _$compile_;
-        $scope = $rootScope.$new();
-    }));
+	beforeEach(inject(function(_$compile_, $rootScope, _$timeout_){
+		$timeout = _$timeout_;
+		$compile = _$compile_;
+		$scope = $rootScope.$new();
+	}));
 
-    it('should focus given elements when appropriate', function(){
-        $scope.activate = false;
+	it('should focus given elements when appropriate', function(){
+		$scope.activate = false;
 
-        element = $compile(angular.element('<div focus-me="activate"></div>'))($scope);
-        $scope.$digest();
+		element = $compile(angular.element('<div focus-me="activate"></div>'))($scope);
+		$scope.$digest();
 
-        spyOn(element[0], 'focus');
-        $scope.activate = true;
-        $scope.$digest();
-        $timeout.flush();
+		spyOn(element[0], 'focus');
+		$scope.activate = true;
+		$scope.$digest();
+		$timeout.flush();
 
-        //make sure the element was given focus
-        expect(element[0].focus).toHaveBeenCalled();
-    });
+		//make sure the element was given focus
+		expect(element[0].focus).toHaveBeenCalled();
+	});
 
-    /* This should call the generateNewPuzzle method with no params
-    it('should blur given elements when appropriate', function(){
-        $scope.activate = false;
+	/* Can't get this one to work
+	it('should blur given elements when appropriate', function(){
+		$scope.activate = false;
 
-        element = $compile(angular.element('<div focus-me="activate"></div>'))($scope);
-        $scope.$digest();
+		element = $compile(angular.element('<div blur="activate"></div>'))($scope);
+		$scope.$digest();
 
-        //$(element[0][0]).blur;
-        spyOn($scope, 'generateNewPuzzle');
-        $scope.activate = false;
-        $scope.$digest();
-        $timeout.flush();
+		spyOn(element[0], 'blur');
+		angular.element(element[0]).triggerHandler('blur');
+		$scope.activate = true;
+		$scope.$digest();
 
-        //make sure the element was given focus
-        expect(element[0][0].blur).toHaveBeenCalled();
-    });
-    */
+		expect(element[0].blur).toHaveBeenCalled();
+	});
+	*/
+
+});
+
+describe('selectMe Directive', function(){
+	var $timeout = null;
+	beforeEach(module('crosswordCreator'));
+
+	beforeEach(inject(function(_$compile_, $rootScope, _$timeout_){
+		$timeout = _$timeout_;
+		$compile = _$compile_;
+		$scope = $rootScope.$new();
+	}));
+
+	it('should select given elements when appropriate', function(){
+		$scope.activate = false;
+
+		element = $compile(angular.element('<div select-me="activate"></div>'))($scope);
+		$scope.$digest();
+
+		spyOn($(element[0]).focus(), 'select');
+		spyOn(element[0], 'focus');
+		$scope.activate = true;
+		$scope.$digest();
+		$timeout.flush();
+
+		//make sure the element was given focus
+		expect(element[0].focus).toHaveBeenCalled();
+	});
+
 });
