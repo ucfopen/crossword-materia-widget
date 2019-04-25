9cd7e822d05b3812fd0110698d807c4ab4abfa59
Nathan Dabu <nathan.dabu@ucf.edu>
2019-04-25 11:05:34 -0400

    switchup helperdocs to compile markdown. *requires updated MWDK

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,80 @@
 const path = require('path')
+const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
+const rules = widgetWebpack.getDefaultRules()
+const copy = widgetWebpack.getDefaultCopyList()
 
-let srcPath = path.join(process.cwd(), 'src')
-let outputPath = path.join(process.cwd(), 'build')
+const srcPath = path.join(process.cwd(), 'src')
+const outputPath = path.join(process.cwd(), 'build')
 
-// load the reusable legacy webpack config from materia-widget-dev
-let webpackConfig = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig({
-	//pass in extra files for webpack to copy
-	preCopy: [
-		{
-			from: `${srcPath}/classList.min.js`,
-			to: outputPath,
-		},
-		{
-			from: `${srcPath}/hammer.min.js`,
-			to: outputPath,
-		},
-		{
-			from: `${srcPath}/_helper-docs`,
-			to: `${outputPath}/_helper-docs`,
-			toType: 'dir'
-		},
-	]
-})
+const customCopy = copy.concat([
+	{
+		from: `${srcPath}/classList.min.js`,
+		to: outputPath,
+	},
+	{
+		from: `${srcPath}/hammer.min.js`,
+		to: outputPath,
+	},
+	{
+		from: `${srcPath}/_helper-docs/assets`,
+		to: `${outputPath}/guides/assets`,
+		toType: 'dir'
+	}
+])
 
-webpackConfig.entry['scoreScreen.js'] = [path.join(__dirname, 'src', 'scoreScreen.coffee')]
-webpackConfig.entry['scoreScreen.css'] = [
-	path.join(__dirname, 'src', 'scoreScreen.html'),
-	path.join(__dirname, 'src', 'scoreScreen.scss')
+const customRules = [
+	rules.loaderDoNothingToJs,
+	rules.loaderCompileCoffee,
+	rules.copyImages,
+	rules.loadHTMLAndReplaceMateriaScripts,
+	rules.loadAndPrefixCSS,
+	rules.loadAndPrefixSASS,
+	rules.loadGuideTemplate
 ]
 
-webpackConfig.entry['print.js'] = [path.join(__dirname, 'src', 'print.coffee')]
-webpackConfig.entry['creator.puzzle.js'] = [path.join(__dirname, 'src', 'creator.puzzle.coffee')]
+const entries = {
+	'creator.js': [
+		path.join(__dirname, 'src', 'creator.coffee')
+	],
+	'player.js': [
+		path.join(__dirname, 'src', 'player.coffee')
+	],
+	'creator.css': [
+		path.join(__dirname, 'src', 'creator.html'),
+		path.join(__dirname, 'src', 'creator.scss')
+	],
+	'player.css': [
+		path.join(__dirname, 'src', 'player.html'),
+		path.join(__dirname, 'src', 'player.scss')
+	],
+	'scoreScreen.js': [
+		path.join(__dirname, 'src', 'scoreScreen.coffee')
+	],
+	'scoreScreen.css': [
+		path.join(__dirname, 'src', 'scoreScreen.html'),
+		path.join(__dirname, 'src', 'scoreScreen.scss')
+	],
+	'print.js': [
+		path.join(__dirname, 'src', 'print.coffee')
+	],
+	'creator.puzzle.js': [
+		path.join(__dirname, 'src', 'creator.puzzle.coffee')
+	],
+	'guides/guideStyles.css': [
+		path.join(__dirname, 'src', '_helper-docs', 'guideStyles.scss')
+	],
+	'guides/creator.html': [
+		__dirname + '/src/_helper-docs/creatorTemplate.html'
+	],
+	'guides/player.html': [
+		__dirname + '/src/_helper-docs/playerTemplate.html'
+	]
+}
 
-module.exports = webpackConfig
+const options = {
+	moduleRules: customRules,
+	copyList: customCopy,
+	entries: entries
+}
+
+module.exports = widgetWebpack.getLegacyWidgetBuildConfig(options)
